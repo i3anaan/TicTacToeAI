a1b9bc9c9bb3f80990417728c4b78c3b011a7c04
I3anaan <I3anaan@live.nl>
2014-06-12 13:40:14 +0200

    Hopefully made compatible with Board class

--- src/ai/QLearningAI.java
+++ src/ai/QLearningAI.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 
 public class QLearningAI implements AI{
 
-	private HashMap<char[],double[]> knowledge = new HashMap<char[],double[]>();
+	private HashMap<char[],double[]> knowledge = new HashMap<Board,double[]>(); //TODO save this knowledge.
 	private static final double GAMMA = 0.9;		//Used in QValue function.
 	private static final char MARK_PLAYER = 'X';	//The mark this AI has
 	private static final char MARK_EMPTY = ' ';		//Empty place mark
 	
-	public int doMove(char[] oldBoard){
+	public int doMove(Board oldBoard){
 		int bestMove = getBestAction(oldBoard); //Calculate best move
 		if(Math.random()>0.9){	//Randomness for exploration.
 			int moveFound = -1;
@@ -22,33 +22,33 @@ public class QLearningAI implements AI{
 			}
 			bestMove = moveFound;
 		}
-		char[] newBoard = updateBoard(oldBoard, bestMove);	//Play best move on clone board
+		Board newBoard = updateBoard(oldBoard.boardClone(), bestMove);	//Play best move on clone board
 		double reward = getReward(oldBoard, newBoard);	//Calculate reward for the transition
 		updateQValue(oldBoard,bestMove,reward);	//Update the QValue for the oldBoard + transition just done.
-		return 3;
+		return bestMove;
 	}
 	
 	
-	public double getQValue(char[] board, int move){
+	public double getQValue(Board board, int move){
 		return knowledge.get(board)[move];
 	}
 	
-	public void updateQValue(char[] board, int move, double reward){
+	public void updateQValue(Board board, int move, double reward){
 		double[] values =knowledge.get(board);
 		values[move] = recalculateQValue(board, move, reward);
 		knowledge.put(board, values);
 	}
 	
-	public double recalculateQValue(char[] board, int move, double reward){
-		char[] newBoard = updateBoard(board,move);
+	public double recalculateQValue(Board board, int move, double reward){
+		Board newBoard = updateBoard(board,move);
 		return reward + GAMMA*getQValue(newBoard,getBestAction(newBoard));
 	}
 	
-	public int getBestAction(char[] board){
+	public int getBestAction(Board board){
 		ArrayList<Integer> bestActions = new ArrayList<Integer>();
 		double bestQValue = Double.MIN_VALUE;
 		for(int i=0;i<9;i++){
-			if(board[i]!=MARK_EMPTY){
+			if(board.get(i)!=MARK_EMPTY){
 				double qValue = getQValue(board, i);
 				if(qValue==bestQValue){
 					bestActions.add(i);
@@ -63,11 +63,12 @@ public class QLearningAI implements AI{
 	}
 	
 	public char[] updateBoard(char[] board, int move){
-		board[move] = MARK_PLAYER;
+		board.doMove(move) = MARK_PLAYER;
 		return board;
 	}
 	
-	public double getReward(char[] boardOld, char[] boardNew){
+	public double getReward(Board boardOld, Board boardNew){
+		//TODO
 		return 0;
 	}
 }
